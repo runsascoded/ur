1cc83488d17a66718b068f870ce7b6fb7f669cfd
Ryan Williams <ryan@runsascoded.com>
2020-05-31 00:59:20 +0000

    add -f option

--- .github/actions/convert/convert.py
+++ .github/actions/convert/convert.py
@@ -31,6 +31,7 @@ def main():
   parser.add_argument('-u', '--user', required=False, help='user.name for Git commit')
   parser.add_argument('-e', '--email', required=False, help='user.email for Git commit')
   parser.add_argument('-b', '--branch', help='Current Git branch (and push target for any changes)')
+  parser.add_argument('-f', '--force', help='Run nbconvert on .ipynb files even if they don\'t seem changed since the base revision')
   parser.add_argument('--fmt', default='markdown', help='Format to convert files to (passed to nbconvert; default: markdown)')
   parser.add_argument('--remote', required=False, help='Git remote to push changes to; defaults to the only git remote, where applicable')
   parser.add_argument('path', nargs='*', help='.ipynb paths to convert')
@@ -65,6 +66,11 @@ def main():
   changed_nbs = lines(['git','diff','--name-only',revision,'--'] + paths)
   if changed_nbs:
     print(f'Found notebook diffs: {changed_nbs}')
+
+  if args.force:
+    nbs = paths
+  else:
+    nbs = changed_nbs
 
   for path in changed_nbs:
     name = path.rsplit('.', 1)[0]

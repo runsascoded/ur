ea2c6ad5a67bbb5da8d172d585d23924658cf11f
Ryan Williams <ryan@runsascoded.com>
2020-05-31 02:59:01 +0000

    log git graph

--- .github/actions/convert/convert.py
+++ .github/actions/convert/convert.py
@@ -102,6 +102,7 @@ def main():
     run('git','config','user.email',email)
     run('git','commit','-a','-m',msg)
     run('git', 'remote', 'set-url', remote, f'https://x-access-token:{token}@github.com/{args.repository}')
+    run('git','log','--oneline','--graph')
     run('git','push',remote,f'HEAD:{branch}')
   else:
     print(f'{len(paths)} notebooks already up-to-date')

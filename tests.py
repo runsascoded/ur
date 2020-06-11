1364c9973c9b0100eecd8f94391ba126262e6067
Ryan Williams <ryan@runsascoded.com>
2020-06-11 04:22:50 +0000

    fix tests.py

--- tests.py
+++ tests.py
@@ -1,28 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Run `ur` notebook tests
-
-# In[16]:
-
-
-from sys import executable as python
-get_ipython().system('{python} -m pip install -q -e .')
-
-
-# Papermill input params:
-
-# In[8]:
-
-
 out = None  # directory to write executed notebooks to
 kernel = '3.8.2'
-
-
-# Default output dir to `out/`:
-
-# In[3]:
-
 
 from pathlib import Path
 cwd = Path.cwd()
@@ -31,14 +8,7 @@ if out:
 else:
     out = cwd / 'out'
 
-
-# Execute notebooks:
-
-# In[7]:
-
-
 from papermill import execute_notebook
 for nb in cwd.glob('*-test.ipynb'):
     print(f'Executing nb: {nb}…')
     execute_notebook(str(nb), str(out / nb.name), nest_asyncio=True, kernel_name=kernel)
-

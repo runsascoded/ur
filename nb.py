715c551ddc950630d6837d4860f790d6a0e04b78
Ryan Williams <ryan@runsascoded.com>
2021-01-02 21:15:01 -0500

    patch nb version

--- nb.py
+++ nb.py
@@ -4,6 +4,6 @@ from pathlib import Path
 
 
 def read_nb(node):
-  nb_version = nbformat.version_info[0]
+  nb_version = nbformat.current_nbformat
   nb = nbformat.reads(node.read_text(), nb_version, encoding=opts.encoding)
   return nb
